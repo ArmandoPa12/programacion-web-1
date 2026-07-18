60f198b483ed2fc6425d16a35d0c4fb656dd843a
ArmandoPa12 <said201133@gmail.com>
2026-07-17 21:57:41 -0400

    el login se conecta con el back y devuleve el JWT

---
+++ web-api-deportivo/web-app-deportivo/Models/LoginDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_app_deportivo.Models
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Formato de correo inválido")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    // Lo que te devuelve la API (La respuesta completa)
+    public class LoginResponseDto
+    {
+        public string Mensaje { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public UsuarioInfoDto Usuario { get; set; } = new();
+    }
+
+    // El objeto interno con los datos del usuario
+    public class UsuarioInfoDto
+    {
+        public int Id { get; set; }
+        public string Nombre_Completo { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Rol { get; set; } = string.Empty;
+    }
+}

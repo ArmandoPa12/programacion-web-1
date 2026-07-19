fdc2983f03fb2cd4363c9f757f978ac208cb2005
ArmandoPa12 <said201133@gmail.com>
2026-07-19 12:32:44 -0400

    inicio de sesion exitoso
    Las rutas estan bloqueadas ahora si no estas ligeado no se podra a acceder a ningun lado

--- web-api-deportivo/web-app-deportivo/Program.cs
+++ web-api-deportivo/web-app-deportivo/Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using web_app_deportivo;
+using web_app_deportivo.Service;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Modifica la línea del HttpClient para que quede así:
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7079/")
 });
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<CustomAuthStateProvider>());
 
 await builder.Build().RunAsync();
---
+++ web-api-deportivo/web-app-deportivo/Service/CustomAuthStateProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+
+namespace web_app_deportivo.Service
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public CustomAuthStateProvider(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "authToken");
+
+            var identity = new ClaimsIdentity();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                identity = new ClaimsIdentity(authenticationType: "jwt");
+            }
+
+            var user = new ClaimsPrincipal(identity);
+            return new AuthenticationState(user);
+        }
+
+        public void NotifyUserAuthentication()
+        {
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public void NotifyUserLogout()
+        {
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
